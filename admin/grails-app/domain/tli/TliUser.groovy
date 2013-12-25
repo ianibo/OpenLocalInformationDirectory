96d6bbac4d0c47fccf5ea721b980bca9816baac1
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-12-25 13:53:47 +0000

    Made user subclass of party

--- admin/grails-app/domain/tli/TliUser.groovy
+++ admin/grails-app/domain/tli/TliUser.groovy
@@ -1,43 +1,43 @@
 package tli
 
-class TliUser {
+class TliUser extends TliParty {
 
-	transient springSecurityService
+  transient springSecurityService
 
-	String username
-	String password
-        String email
-	boolean enabled = true
-	boolean accountExpired
-	boolean accountLocked
-	boolean passwordExpired
+  String username
+  String password
+  String email
+  boolean enabled = true
+  boolean accountExpired
+  boolean accountLocked
+  boolean passwordExpired
 
-	static transients = ['springSecurityService']
+  static transients = ['springSecurityService']
 
-	static constraints = {
-		username blank: false, unique: true
-		password blank: false
-	}
+  static constraints = {
+    username blank: false, unique: true
+    password blank: false
+  }
 
-	static mapping = {
-		password column: '`password`'
-	}
+  static mapping = {
+    password column: '`password`'
+  }
 
-	Set<TliRole> getAuthorities() {
-		TliUserTliRole.findAllByTliUser(this).collect { it.tliRole } as Set
-	}
+  Set<TliRole> getAuthorities() {
+    TliUserTliRole.findAllByTliUser(this).collect { it.tliRole } as Set
+  }
 
-	def beforeInsert() {
-		encodePassword()
-	}
+  def beforeInsert() {
+    encodePassword()
+  }
 
-	def beforeUpdate() {
-		if (isDirty('password')) {
-			encodePassword()
-		}
-	}
+  def beforeUpdate() {
+    if (isDirty('password')) {
+      encodePassword()
+    }
+  }
 
-	protected void encodePassword() {
-		password = springSecurityService.encodePassword(password)
-	}
+  protected void encodePassword() {
+    password = springSecurityService.encodePassword(password)
+  }
 }
