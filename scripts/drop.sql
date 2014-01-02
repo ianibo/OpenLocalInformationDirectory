26afb1bd98b2ed5954d1b347f0f51265fb2a9254
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-01-02 10:26:06 +0000

    Work on drop script

--- scripts/drop.sql
+++ scripts/drop.sql
@@ -1,10 +1,13 @@
+SET foreign_key_checks = 0;
 drop table if exists affiliation;
 drop table if exists directory_entry;
 drop table if exists lis_authority;
 drop table if exists registration_code;
 drop table if exists tli_user_tli_role;
 drop table if exists tli_role;
-drop table if exists tli_party
+drop table if exists tli_party;
+drop table if exists tli_collection;
 drop table if exists refdata_value;
 drop table if exists refdata_category;
-
+drop table if exists refdata_relation;
+SET foreign_key_checks = 1;
