6b9326fda5f78f47f3638c80a7e78b0f8667fe13
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-04-05 13:52:22 +0100

    Work on shortcode service

--- admin/grails-app/domain/tli/DirectoryEntryShortcode.groovy
+++ admin/grails-app/domain/tli/DirectoryEntryShortcode.groovy
@@ -10,4 +10,23 @@ class DirectoryEntryShortcode {
 
   static constraints = {
   }
+
+  static generateShortcode(entry, base) {
+    def base_shortcode = base.trim().toLowerCase().replaceAll("\\p{Punct}","").trim().replaceAll("\\W","_")
+    def shortcode = base_shortcode
+    def located_record = domain_class.getClazz().executeQuery("select count(o) from DirectoryEntry as o where o.title = ?",shortcode)[0]
+    def result = null
+
+    int ctr = 0;
+
+    // See if initial_guess is already used
+    while ( located_record > 0 ) {
+      shortcode = base_shortcode+'_'+ctr
+      located_record = domain_class.getClazz().executeQuery("select count(o) from ${baseclass} as o where o.${field} = ?",shortcode)[0]
+    }
+
+    result = new DirectoryEntryShortcode(dirent:entry,shortcode:shortcode).save()
+
+    return result
+  }
 }
