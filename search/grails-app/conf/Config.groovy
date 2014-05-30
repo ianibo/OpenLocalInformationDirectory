d3e1cb650c6f8e8d52ff4951455b6976367d46d2
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-05-30 12:17:35 +0100

    Solid progress on fb oauth login

--- search/grails-app/conf/Config.groovy
+++ search/grails-app/conf/Config.groovy
@@ -159,7 +159,8 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
         '/**/js/**':                              ['permitAll'],
         '/**/css/**':                             ['permitAll'],
         '/**/images/**':                          ['permitAll'],
-        '/**/favicon.ico':                        ['permitAll']
+        '/**/favicon.ico':                        ['permitAll'],
+        '/oauth/**':                              ['permitAll']
 ]
 
 // Added by the Spring Security OAuth plugin:
