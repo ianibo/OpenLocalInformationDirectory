24da3ecef3611f89c1d0e4a24f05fb48752b8679
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-13 15:16:01 +0100

    Markerclusterer and spider deps

--- search/grails-app/assets/javascripts/application.js
+++ search/grails-app/assets/javascripts/application.js
@@ -1,3 +1,24 @@
+//= require jquery
+//= require bootstrap
+//= require bootstrap-editable.min
+//= require bootstrap-datetimepicker.min
+//= require editable
+//= require moment.min
+//= require select2.min
+//= require oms.min.js
+//= require markerclusterer.js
+
+if (typeof jQuery !== 'undefined') {
+        (function($) {
+                $('#spinner').ajaxStart(function() {
+                        $(this).fadeIn();
+                }).ajaxStop(function() {
+                        $(this).fadeOut();
+                });
+        })(jQuery);
+}
+
+
 if (typeof jQuery !== 'undefined') {
 	(function($) {
 		$('#spinner').ajaxStart(function() {
