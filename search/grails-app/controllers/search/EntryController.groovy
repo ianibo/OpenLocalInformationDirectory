46601e76f08a3aebd5e93796dff079f1f19f21d0
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-31 16:50:07 +0100

    updated

--- search/grails-app/controllers/search/EntryController.groovy
+++ search/grails-app/controllers/search/EntryController.groovy
@@ -1,14 +1,5 @@
 package search
 
-import grails.converters.*
-import groovy.xml.MarkupBuilder
-import org.codehaus.groovy.grails.commons.ApplicationHolder
-import tli.*;
-
-import grails.plugin.springsecurity.annotation.Secured
-import org.codehaus.groovy.grails.commons.GrailsClassUtils
-import me.ianibbo.common.*
-
 class EntryController {
 
   def newGazetteerService
