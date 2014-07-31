2b207cd8b04bf480e548d9cf9b96e704d45dcd76
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-07-31 16:28:31 +0100

    Pre dev merge commit

--- scripts/esreset.sh
+++ scripts/esreset.sh
@@ -6,3 +6,15 @@ curl -XDELETE 'http://localhost:9200/olid'
 curl -X PUT "localhost:9200/olid" -d '{
   "settings" : {}
 }'
+
+# curl -X PUT "localhost:9200/kbplus/com.k_int.kbplus.TitleInstance/_mapping" -d '{
+#   "com.k_int.kbplus.TitleInstance" : {
+#     "properties" : {
+#       "title" : {
+#         type : "string",
+#         analyzer : "snowball"
+#       }
+#     }
+#   }
+# }'
+
