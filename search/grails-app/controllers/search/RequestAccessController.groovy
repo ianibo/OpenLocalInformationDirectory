eb4a92130bef6d4c8bc1c30f51a9377e0dbd79be
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-01 19:59:05 +0100

    Working on email when requesting resources

--- search/grails-app/controllers/search/RequestAccessController.groovy
+++ search/grails-app/controllers/search/RequestAccessController.groovy
@@ -109,6 +109,27 @@ class RequestAccessController {
 
   def private emailRecordOwnersForPermission() {
     log.debug("emailRecordOwnersForPermission()");
+
+    // dear <<emailaddress>>,
+    //
+    // This is the data controller at <<app.name>>. Recently, a user with Name <<name>> and email address <<email>> has requested access
+    // to edit <resource.url> <<resource.title>> in our system. This email address is receiving this email because it is listed as the owner of the
+    // information, and we would like you to confirm if this user should be allowed to edit this information.
+
+    // THere are two ways ways to manage this process 
+
+    // 1) click THIS link, which will ask you to log in to the system and manage this request
+
+    // 2) Select one of the following options: 
+
+    // I don't want to approve this request, and please don't bother me again - (An administrator may subseqently authorize a user to edit this data 
+    // if they can demonstrate that they have a legitimate reason to do so).
+
+    // I want to approve this request, and I'm happy to approve future requests
+
+    // I want to approve this request, please make this user responsible for all future requests
+
+   
   }
 
 }
