1de757334a87256c87c2967a705c61b9f27dbc7c
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-30 14:52:00 +0100

    upped dependency on auth module

--- oliddm/grails-app/conf/BuildConfig.groovy
+++ oliddm/grails-app/conf/BuildConfig.groovy
@@ -45,7 +45,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-      compile ":authmodel:0.4.1"
+      compile ":authmodel:0.4.2"
 
       build(":release:3.0.1", ":rest-client-builder:1.0.3") {
             export = false
